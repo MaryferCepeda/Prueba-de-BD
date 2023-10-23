4df8c26725a6f5b6c5a886055c2c37a72f3b8967
MaryferCepeda <maryfer1254@gmail.com>
2023-10-23 07:44:22 -0600

    Creacion del proyecto de prueba

---
+++ src/prubas_de_bd/Prubas_De_BD.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prubas_de_bd;
+
+/**
+ *
+ * @author HOGAR
+ */
+public class Prubas_De_BD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
